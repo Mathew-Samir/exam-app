5835d40296dc741e80369cf630ec11560e11c0fa
Mathew-Samir <mthwsamir@gmail.com>
2026-03-04 23:33:03 +0200

    feat: 🎸 Implementing login & signup as ui

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -1,8 +1,12 @@
 import {Component} from "@angular/core";
+import { Button } from "../../../../shared/components/ui/button/button";
+import { InputComponent } from "../../../../shared/components/ui/input/input";
+import { RouterLink } from "@angular/router";
+
 
 @Component({
     selector: "app-login",
-    imports: [],
+    imports: [Button,InputComponent,RouterLink],
     templateUrl: "./login.html",
     styleUrl: "./login.scss",
 })
--- src/app/features/auth/components/sign-up/sign-up.ts
+++ src/app/features/auth/components/sign-up/sign-up.ts
@@ -1,8 +1,12 @@
 import {Component} from "@angular/core";
+import { Button } from "../../../../shared/components/ui/button/button";
+import { InputComponent } from "../../../../shared/components/ui/input/input";
+import { RouterLink } from "@angular/router";
+
 
 @Component({
     selector: "app-sign-up",
-    imports: [],
+    imports: [Button,InputComponent,RouterLink],
     templateUrl: "./sign-up.html",
     styleUrl: "./sign-up.scss",
 })
