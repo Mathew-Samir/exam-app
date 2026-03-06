6fe093a084974eb4e635f823f2c22ab780234ad7
Mathew-Samir <mthwsamir@gmail.com>
2026-03-06 03:06:49 +0200

    refactor: 💡 routes , font , shard styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/auth/components/login/login'
-import { ResetPassword } from './features/auth/components/reset-password/reset-password';
-import { SignUp } from './features/auth/components/sign-up/sign-up';
+
+
 
 
 export const routes: Routes = [
-    { path: "",component: Login },
-    { path: "login",component: Login },
-    { path: "reset-password",component: ResetPassword },
-    { path: "signup",component: SignUp }
+    { path: "",loadComponent: () => import('./features/auth/components/login/login').then(m => m.Login), title:"Login" },
+    { path: "login",loadComponent: () => import('./features/auth/components/login/login').then(m => m.Login), title:"Login"},
+    { path: "signup",loadComponent: () => import('./features/auth/components/sign-up/sign-up').then(m => m.SignUp), title:"SignUp"},
+    { path: "reset-password",loadComponent: () => import('./features/auth/components/reset-password/reset-password').then(m => m.ResetPassword), title:"Reset Password"},
+
 ];
